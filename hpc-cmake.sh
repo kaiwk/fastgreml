d98cb09d7f5e0923a07aa73586cfa2424f494e84
Wang Kai <kaiwkx@gmail.com>
2025-06-06 18:35:29 +0800

    Fix cmake build

--- hpc-cmake.sh
+++ hpc-cmake.sh
@@ -4,7 +4,7 @@
 module load cmake eigen intelmkl
 
 cmake -DBUILD_ON_HPC=ON \
-    -DCMAKE_PREFIX_PATH="/soft/devtools/eigen-3.4.0:${CMAKE_PREFIX_PATH}" \
+    -DCMAKE_PREFIX_PATH="/soft/devtools/eigen-3.4.0;${CMAKE_PREFIX_PATH}" \
     -DMKL_DIR="/soft/compiler/intel/oneapi-2022.2/mkl/2022.1.0/lib/cmake/mkl" \
     -DCMAKE_BUILD_TYPE=Release -B build/Release -S .
 
