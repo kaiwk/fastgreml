a037798430c694bc3b6b6d489b31f7becfc35745
kai wang <wangkai@intel034.cluster.com>
2025-06-19 14:52:58 +0800

    hpc run script

--- scripts/hpc/hpc-run.sh
+++ scripts/hpc/hpc-run.sh
@@ -4,7 +4,19 @@ echo `date`
 # BUILD_TYPE=Release
 BUILD_TYPE=RelWithDebInfo
 
-perf record --call-graph=dwarf -F 99 -g  \
+export OMP_DISPLAY_ENV=true
+export OMP_NUM_THREADS=24
+export OMP_PROC_BIND=close       # keeps threads close together (better L2/L3 sharing, often better for matrix-heavy tasks)
+export OMP_PLACES=cores          # threads bound to a core
+
+# export MKL_NUM_THREADS=1
+# export MKL_DYNAMIC=false
+
+export KMP_AFFINITY=verbose
+
+# perf record --call-graph=dwarf -F 99 -g  \
+LD_PRELOAD="/storage/yangjianLab/wangkai/Repo/mimalloc/out/Release/libmimalloc.so"  \
+MIMALLOC_VERBOSE=1 \
 ./build/${BUILD_TYPE}/fastgreml --grmlist /storage/yangjianLab/xuting/data/grm/WGS_unrel/sample50k/mgrm_nml_noIG_12group.txt \
 --mphe 1,/storage/yangjianLab/xuting/data/phe/50.pheno \
 --cov /storage/yangjianLab/xuting/ukb/UKB_All_covariates.covar \
